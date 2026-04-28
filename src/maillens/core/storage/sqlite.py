e3ac7bacb7d2b3e9e8da4e69d1245f2e8d117fb9
GBDJXB <lexinrong416@gmail.com>
2026-04-27 22:03:04 -0700

    feat: add attachments and message_recipients Table

--- src/maillens/core/storage/sqlite.py
+++ src/maillens/core/storage/sqlite.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Table, MetaData, Column, String, Text, DateTime, ForeignKey
+from sqlalchemy import Table, MetaData, Column, String, Text, DateTime, ForeignKey, Integer
+
 
 metadata = MetaData()
 
@@ -6,6 +7,15 @@ contacts = Table(
     "Contacts", metadata,
     Column("email", String, nullable=False, primary_key=True),
     Column("name", String)
+)
+
+attachments = Table(
+    "Attachments", metadata,
+    Column("id", Integer, primary_key=True, autoincrement=True),
+    Column("message_id", ForeignKey("Messages.message_id")),
+    Column("filename", String),
+    Column("content_type", String),
+    Column("size_bytes", Integer)
 )
 
 messages = Table(
@@ -29,4 +39,12 @@ messages = Table(
     Column("body_html", Text),
     # Labels
     Column("folder", String),
+)
+
+message_recipients = Table(
+    "Message_recipients", metadata,
+    Column("message_id", ForeignKey("Messages.message_id"), primary_key=True),
+    Column("email", ForeignKey("Contacts.email"), primary_key=True),
+    Column("name", String),
+    Column("kind", String)
 )
